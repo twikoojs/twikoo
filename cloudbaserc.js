25862ca7ff319aba078f7e9bec588ec81997f10c
iMaeGoo <mail1st@qq.com>
2020-11-19 15:18:24 +0800

    fix(deploy): 修复Win10字符编码导致的部署失败

--- cloudbaserc.js
+++ cloudbaserc.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 let envId
 try {
   envId = fs.readFileSync('./envId.txt').toString()
-  envId = envId.trim().replace(/[^0-9a-zA-Z]+/g, '')
+  envId = envId.trim().replace(/[^0-9a-zA-Z\-]+/g, '')
 } catch (e) {
   throw new Error('无法读取环境id，请先配置 envId.txt - https://twikoo.js.org/quick-start.html')
 }

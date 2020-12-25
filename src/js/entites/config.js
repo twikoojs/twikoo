239e1809560974f5c9911a41c9683e1bf63f8570
iMaeGoo <mail1st@qq.com>
2020-12-25 16:19:12 +0800

    fix(0.4.4): 修复问题 * 新增 未发送的评论在刷新页面时不会丢失 * 修复 点击表情选择器外部无法关闭表情选择器

--- src/js/entites/config.js
+++ src/js/entites/config.js
@@ -5,7 +5,24 @@ class Config {
   constructor (model = {}) {
     this.SITE_NAME = model.SITE_NAME
     this.SITE_URL = model.SITE_URL
+    this.COMMENT_PAGE_SIZE = model.COMMENT_PAGE_SIZE
+    this.MASTER_TAG = model.MASTER_TAG
+    this.COMMENT_BG_IMG = model.COMMENT_BG_IMG
+    this.GRAVATAR_CDN = model.GRAVATAR_CDN
+    this.SHOW_IMAGE = model.SHOW_IMAGE
+    this.IMAGE_CDN = model.IMAGE_CDN
+    this.SHOW_EMOTION = model.SHOW_EMOTION
+    this.EMOTION_CDN = model.EMOTION_CDN
+    this.COMMENT_PLACEHOLDER = model.COMMENT_PLACEHOLDER
+    this.REQUIRED_FIELDS = model.REQUIRED_FIELDS
+    this.QCLOUD_SECRET_ID = model.QCLOUD_SECRET_ID
+    this.QCLOUD_SECRET_KEY = model.QCLOUD_SECRET_KEY
+    this.LIMIT_PER_MINUTE = model.LIMIT_PER_MINUTE
+    this.SC_MAIL_NOTIFY = model.SC_MAIL_NOTIFY
     this.SMTP_SERVICE = model.SMTP_SERVICE
+    this.SMTP_HOST = model.SMTP_HOST
+    this.SMTP_PORT = model.SMTP_PORT
+    this.SMTP_SECURE = model.SMTP_SECURE
     this.SMTP_USER = model.SMTP_USER
     this.SMTP_PASS = model.SMTP_PASS
     this.SC_SENDKEY = model.SC_SENDKEY
@@ -14,6 +31,10 @@ class Config {
     this.SENDER_EMAIL = model.SENDER_EMAIL
     this.BLOGGER_EMAIL = model.BLOGGER_EMAIL
     this.AKISMET_KEY = model.AKISMET_KEY
+    this.MAIL_SUBJECT = model.MAIL_SUBJECT
+    this.MAIL_TEMPLATE = model.MAIL_TEMPLATE
+    this.MAIL_SUBJECT_ADMIN = model.MAIL_SUBJECT_ADMIN
+    this.MAIL_TEMPLATE_ADMIN = model.MAIL_TEMPLATE_ADMIN
   }
 }
 

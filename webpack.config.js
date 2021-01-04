ef911c2ecbff4d0c1fe4919e0b0975ecbab11fc0
iMaeGoo <mail1st@qq.com>
2021-01-04 09:25:17 +0800

    fix: coding style

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const ROOT_PATH = path.resolve(__dirname)
 const BUILD_PATH = path.resolve(ROOT_PATH, 'dist')
 const version = require('./package.json').version
-const TerserPlugin = require("terser-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin')
 const banner =
   'Twikoo v' + version + '\n' +
   '(c) 2020-' + new Date().getFullYear() + ' iMaeGoo\n' +
@@ -18,7 +18,7 @@ module.exports = {
       { test: /\.vue$/, loader: 'vue-loader' },
       { test: /\.css$/, use: ['vue-style-loader', 'css-loader'] },
       { test: /\.svg$/, loader: 'svg-inline-loader' },
-      { test: /\.js$/, use: { loader: 'babel-loader', options: { presets: ['@babel/preset-env'] ,plugins: ["@babel/plugin-transform-modules-commonjs","@babel/transform-runtime"]} } }
+      { test: /\.js$/, use: { loader: 'babel-loader', options: { presets: ['@babel/preset-env'], plugins: ['@babel/plugin-transform-modules-commonjs', '@babel/transform-runtime'] } } }
     ]
   },
   entry: {
@@ -42,27 +42,27 @@ module.exports = {
     }),
     new VueLoaderPlugin(),
     new TerserPlugin({
-	  parallel: 4,
-	  terserOptions: {
-	    ecma: 5,
-	    toplevel: true,
-	    ie8: true,
-	    safari10: true,
-     },
+      parallel: 4,
+      terserOptions: {
+        ecma: 5,
+        toplevel: true,
+        ie8: true,
+        safari10: true
+      }
     })
   ],
   devServer: {
     static: [{
-        directory: BUILD_PATH,
-        publicPath: '/dist/',
-        serveIndex: true,
-        watch: true,
-    },],
+      directory: BUILD_PATH,
+      publicPath: '/dist/',
+      serveIndex: true,
+      watch: true
+    }],
     port: 9820,
     host: 'localhost',
-    open:true,
-    hot:true,
-    compress: true,
+    open: true,
+    hot: true,
+    compress: true
   },
   performance: {
     maxEntrypointSize: 524288,

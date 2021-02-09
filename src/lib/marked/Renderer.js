c7c6de64636c9f7f8184f96c4c3a69d5d278d826
iMaeGoo <mail1st@qq.com>
2021-02-09 10:31:58 +0800

    * 修复 升级 marked 以修复 CVE-2021-21306 漏洞

--- src/lib/marked/Renderer.js
+++ src/lib/marked/Renderer.js
@@ -35,6 +35,8 @@ module.exports = class Renderer {
       }
     }
 
+    code = code.replace(/\n$/, '') + '\n';
+
     if (!lang) {
       return '<pre><code>'
         + (escaped ? code : escape(code, true))

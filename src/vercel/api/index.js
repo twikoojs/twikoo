db5a7716057d01c6f1348d4a8cd76a701e8a82f8
iMaeGoo <mail1st@qq.com>
2021-12-09 15:36:15 +0800

    fix: do not save empty config

--- src/vercel/api/index.js
+++ src/vercel/api/index.js
@@ -1590,6 +1590,7 @@ async function readConfig () {
 
 // 写入配置
 async function writeConfig (newConfig) {
+  if (!Object.keys(newConfig).length) return 0
   console.log('写入配置：', newConfig)
   try {
     let updated

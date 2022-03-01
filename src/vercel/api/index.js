b1524179ea4a4600adaee24e97fd58a0652a090b
HEIGE-PCloud <heige.pcloud@outlook.com>
2022-03-01 15:34:51 +0000

    feat: change parse_mode to HTML to avoid escaping
    Signed-off-by: HEIGE-PCloud <heige.pcloud@outlook.com>

--- src/vercel/api/index.js
+++ src/vercel/api/index.js
@@ -1122,10 +1122,11 @@ async function noticeTelegram (comment) {
     return
   }
   if (config.BLOGGER_EMAIL === comment.mail) return
-  const pushContent = getIMPushContent(comment, { markdown: true })
+  const pushContent = getIMPushContent(comment)
   const sendResult = await axios.post(`https://api.telegram.org/bot${config.TG_BOT_TOKEN}/sendMessage`, {
-    text: pushContent.subject + '\n' + pushContent.content,
-    chat_id: config.TG_CHAT_ID
+    text: pushContent.subject + pushContent.content,
+    chat_id: config.TG_CHAT_ID,
+    parse_mode: 'HTML'
   })
   console.log('Telegram 通知结果：', sendResult)
 }

695162de5b2edec071bd11e4ac7f6b66e3f8177a
Wmz1024 <82153828+wmz1024@users.noreply.github.com>
2022-05-18 20:23:29 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,7 +14,7 @@ module.exports = {
       // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
       repo: 'imaegoo/twikoo',
       // 假如文档不是放在仓库的根目录下：
-      docsDir: 'docs/en',
+      docsDir: 'docs',
       // 假如文档放在一个特定的分支下：
       docsBranch: 'main',},
       '/':{

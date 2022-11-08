a805afdb32c30a4ed85613de607be2ef5238da03
Mercury233 <me@mercury233.me>
2022-11-08 08:31:19 +0800

    add TWIKOO_LOCALHOST_ONLY

--- src/server/self-hosted/server.js
+++ src/server/self-hosted/server.js
@@ -30,7 +30,8 @@ server.on('request', async function (request, response) {
 })
 
 const port = parseInt(process.env.TWIKOO_PORT) || 8080
+const host = process.env.TWIKOO_LOCALHOST_ONLY ? '::1/128' : '::'
 
-server.listen(port, function () {
+server.listen(port, host, function () {
   console.log(`Twikoo function started on port ${port}`)
 })

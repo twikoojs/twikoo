69115a60936bbd97fac16ad3079c65c61b1e7040
Mercury233 <me@mercury233.me>
2022-11-11 09:16:08 +0800

    add TWIKOO_LOCALHOST_ONLY (#481)
    * add TWIKOO_LOCALHOST_ONLY
    
    * Update README.md
    
    * Update server.js
    
    * Update server.js
    
    * Update README.md

--- src/server/self-hosted/README.md
+++ src/server/self-hosted/README.md
@@ -19,3 +19,4 @@ tkserver
 | `TWIKOO_DATA` | 数据库存储路径 | `./data` |
 | `TWIKOO_PORT` | 端口号 | `8080` |
 | `TWIKOO_THROTTLE` | IP 请求限流，当同一 IP 短时间内请求次数超过阈值将对该 IP 返回错误 | `250` |
+| `TWIKOO_LOCALHOST_ONLY` | 为`true`时只监听本地请求，使得 nginx 等服务器反代之后不暴露原始端口 | `null` |
--- src/server/self-hosted/server.js
+++ src/server/self-hosted/server.js
@@ -30,7 +30,8 @@ server.on('request', async function (request, response) {
 })
 
 const port = parseInt(process.env.TWIKOO_PORT) || 8080
+const host = process.env.TWIKOO_LOCALHOST_ONLY === 'true' ? 'localhost' : '::'
 
-server.listen(port, function () {
-  console.log(`Twikoo function started on port ${port}`)
+server.listen(port, host, function () {
+  console.log(`Twikoo function started on host ${host} port ${port}`)
 })

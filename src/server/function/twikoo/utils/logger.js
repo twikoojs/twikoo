d9c57a7644757c6e55a2d997c64f5925f3c50b81
mercury233 <me@mercury233.me>
2023-08-30 14:35:28 +0800

    lint

--- src/server/function/twikoo/utils/logger.js
+++ src/server/function/twikoo/utils/logger.js
@@ -1,23 +1,19 @@
 let envLogLevel = process.env.TWIKOO_LOG_LEVEL || 'info'
 envLogLevel = envLogLevel.toLowerCase()
-const logLevel = { 'verbose': 1, 'info': 2, 'warn': 3, 'error': 4 }[envLogLevel] || 2
+const logLevel = { verbose: 1, info: 2, warn: 3, error: 4 }[envLogLevel] || 2
 
 const logger = {
   log: (...messages) => {
-    if (logLevel <= 1)
-      console.log(logPrefix(), ...messages)
+    if (logLevel <= 1) console.log(logPrefix(), ...messages)
   },
   info: (...messages) => {
-    if (logLevel <= 2)
-      console.info(logPrefix(), ...messages)
+    if (logLevel <= 2) console.info(logPrefix(), ...messages)
   },
   warn: (...messages) => {
-    if (logLevel <= 3)
-      console.warn(logPrefix(), ...messages)
+    if (logLevel <= 3) console.warn(logPrefix(), ...messages)
   },
   error: (...messages) => {
-    if (logLevel <= 4)
-      console.error(logPrefix(), ...messages)
+    if (logLevel <= 4) console.error(logPrefix(), ...messages)
   }
 }
 

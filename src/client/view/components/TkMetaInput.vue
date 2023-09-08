d6b68e75b3170d8e007d42f4f2ad4f6513bd0926
iMaeGoo <mail1st@qq.com>
2023-09-08 17:40:51 +0800

    fix: update qq name api

--- src/client/view/components/TkMetaInput.vue
+++ src/client/view/components/TkMetaInput.vue
@@ -91,7 +91,7 @@ export default {
       if (isQQ(this.metaData.nick)) {
         // 模仿 Valine 的操作逻辑，当用户在 [昵称] 输入 QQ 号时
         // 1. 自动填充数字 QQ 邮箱到 [邮箱]
-        // 2. 自动填充 QQ 昵称到 [昵称] (使用了 https://docs.tenapi.cn/ 提供的接口，感谢作者：I Am I)
+        // 2. 自动填充 QQ 昵称到 [昵称]
         // 3. 自动显示 QQ 头像
         const qqNum = this.metaData.nick.replace(/@qq.com/ig, '')
         const qqMail = `${qqNum}@qq.com`
@@ -100,7 +100,7 @@ export default {
       }
     },
     getQQNick (qqNum) {
-      const url = `https://tenapi.cn/qqname?qq=${qqNum}`
+      const url = `https://api.qjqq.cn/api/qqinfo?qq=${qqNum}`
       const xhr = new XMLHttpRequest()
       xhr.onreadystatechange = () => {
         if (xhr.readyState === 4 && xhr.status === 200) {

9f9c4bb719838b9b6ac0758d9b9a0a741bfdb0fd
iMaeGoo <mail1st@qq.com>
2023-11-27 15:36:44 +0800

    doc: 点赞回复按钮点击会回到顶部

--- docs/.vitepress/theme/Twikoo.vue
+++ docs/.vitepress/theme/Twikoo.vue
@@ -58,7 +58,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="comment-container">
+  <div class="comment-container vp-raw">
     <!-- KaTeX -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css" integrity="sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X" crossorigin="anonymous">
     <component :is="'script'" defer src="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.js" integrity="sha384-g7c+Jr9ZivxKLnZTDUhnkOnsh30B4H0rpLUpJ4jAIKs4fnJI+sEnkvrMWph2EDg4" crossorigin="anonymous"></component>

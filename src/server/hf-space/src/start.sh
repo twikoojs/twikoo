51296487291a10178186bc9687dbceeef4e28e67
星野梦月 <147403913+Hoshino-Yumetsuki@users.noreply.github.com>
2024-05-27 21:33:40 +0800

    将cloudflared日志输出到标准输出

--- src/server/hf-space/src/start.sh
+++ src/server/hf-space/src/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-nohup /usr/local/bin/cloudflared tunnel --no-autoupdate run --token $CF_ZERO_TRUST_TOKEN &
+/usr/local/bin/cloudflared tunnel --no-autoupdate run --token $CF_ZERO_TRUST_TOKEN &
 tkserver

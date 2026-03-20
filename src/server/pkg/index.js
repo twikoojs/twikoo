a40e5b7c5498b23f64762cd5fc30a15455b8113c
空巷一人 <1530688385@qq.com>
2026-03-20 11:27:18 +0800

    feat: 修改可执行文件打包方式，适配更高的node版本 (#902)
    * feat: 修改可执行文件打包方式，适配更高的node版本
    
    * fix: lint

--- src/server/pkg/index.js
+++ src/server/pkg/index.js
@@ -1,6 +1,7 @@
 const { program } = require('commander')
 const { join } = require('path')
-const { existsSync, writeFileSync, readFileSync } = require('fs')
+const { existsSync, writeFileSync } = require('fs')
+const { getAsset, isSea } = require('node:sea')
 
 program
   .name(require('./package.json').name)
@@ -9,49 +10,48 @@ program
     `DESCRIPTION:
   Official website: https://twikoo.js.org/`
   )
-  .addHelpCommand(false)
+  .helpCommand(false)
 
 program.parse(process.argv)
 
-// 创建.env文件
-if (
-  process.pkg &&
-  !existsSync(join(process.execPath, '..', '.env')) &&
-  existsSync(join(__dirname, '.env'))
-) {
-  writeFileSync(
-    join(process.execPath, '..', '.env'),
-    readFileSync(join(__dirname, '.env'), 'utf-8')
-  )
-}
+try {
+  if (isSea()) {
+    // 创建.env文件
+    if (!existsSync(join(__dirname, '.env'))) {
+      writeFileSync(join(__dirname, '.env'), getAsset('.env', 'utf8'))
+    }
 
-// 适配iis
-if (
-  process.pkg &&
-  process.platform === 'win32' &&
-  !existsSync(join(process.execPath, '..', './web.config')) &&
-  existsSync(join(__dirname, './web.config'))
-) {
-  writeFileSync(
-    join(process.execPath, '..', './web.config'),
-    readFileSync(join(__dirname, './web.config'), 'utf-8')
-  )
-}
+    // 适配iis
+    if (process.platform === 'win32' && !existsSync(join(__dirname, './web.config'))) {
+      writeFileSync(join(__dirname, './web.config'), getAsset('web.config', 'utf8'))
+    }
+
+    // 拷贝ip2region.db
+    if (!existsSync(join(__dirname, './ip2region.db'))) {
+      writeFileSync(
+        join(__dirname, './ip2region.db'),
+        Buffer.from(new Uint8Array(getAsset('ip2region.db')))
+      )
+    }
+    if (!existsSync(join(__dirname, './xhr-sync-worker.js'))) {
+      writeFileSync(
+        join(__dirname, './xhr-sync-worker.js'),
+        getAsset('xhr-sync-worker.js', 'utf8')
+      )
+    }
+  }
+} catch (error) {}
 
 // 获取env
 require('dotenv').config({
-  path:
-    process.pkg && existsSync(join(process.execPath, '..', '.env'))
-      ? join(process.execPath, '..', '.env')
-      : existsSync(join(__dirname, '.env'))
-        ? join(__dirname, '.env')
-        : undefined
+  override: true,
+  path: existsSync(join(__dirname, '.env')) ? join(__dirname, '.env') : undefined
 })
 
 // 匹配iis
 if (process.env.ASPNETCORE_PORT) {
   process.env.TWIKOO_PORT = process.env.ASPNETCORE_PORT
-  process.env.TWIKOO_LOCALHOST_ONLY = null
+  process.env.TWIKOO_LOCALHOST_ONLY = undefined
 }
 
 require('tkserver')
---
+++ src/server/pkg/tsdown.config.mjs
@@ -0,0 +1,28 @@
+import { defineConfig } from 'tsdown'
+
+export default defineConfig({
+  entry: ['./index.js'],
+  platform: 'node',
+  // ...config options
+  clean: ['dist'],
+  format: 'commonjs',
+  exe: {
+    enabled: true,
+    fileName: 'twikoo',
+    targets: [
+      { platform: 'linux', arch: 'x64', nodeVersion: '25.8.1' },
+      { platform: 'darwin', arch: 'arm64', nodeVersion: '25.8.1' },
+      { platform: 'darwin', arch: 'x64', nodeVersion: '25.8.1' },
+      { platform: 'win', arch: 'x64', nodeVersion: '25.8.1' }
+    ],
+    outDir: 'dist',
+    seaConfig: {
+      assets: {
+        '.env': '.env',
+        'ip2region.db': 'ip2region.db',
+        'xhr-sync-worker.js': 'xhr-sync-worker.js',
+        'web.config': 'web.config'
+      }
+    }
+  }
+})

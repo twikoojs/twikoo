3eaf14324dbb1fb15e667ce1efca455fcdb2e548
iMaeGoo <mail1st@qq.com>
2026-07-06 19:52:24 +0800

    Refactor i18n language handling and translation logic

--- src/client/utils/i18n/index.js
+++ src/client/utils/i18n/index.js
@@ -1,4 +1,4 @@
-import i18n, { idI18n } from './i18n'
+import i18n from './i18n'
 
 // ISO Language Code Table http://www.lingoes.net/en/translator/langcode.htm
 // RSS Language Code Table https://www.rssboard.org/rss-language-codes
@@ -8,61 +8,40 @@ const langs = {
   'zh-cn': 0,
   'zh-hk': 1,
   'zh-tw': 2,
-
-  en: 3,
   'en-us': 3,
   'en-gb': 3,
-
+  en: 3,
   uz: 4,
   'uz-uz': 4,
-
   ja: 5,
   'ja-jp': 5,
-
   ko: 6,
   'ko-kr': 6,
-
   vi: 7,
-  'vi-vn': 7,
-
+  'vi-VN': 7,
   id: 8,
-  'id-id': 8,
-  indonesia: 8,
-  indonesian: 8
+  'id-id': 8
 }
 
 const defaultLanguage = 'zh-cn'
 let twikooLangOption = ''
 
-const hasLang = (lang) => {
-  return Object.prototype.hasOwnProperty.call(langs, lang)
-}
-
 const setLanguage = (options = {}) => {
-  const lang = options.lang?.toLowerCase()
-
-  if (lang && hasLang(lang)) {
-    twikooLangOption = lang
+  if (options.lang && options.lang.toLowerCase() in langs) {
+    twikooLangOption = options.lang
   }
 }
 
 const translate = (key, language) => {
-  const lang = (
-    language ||
-    twikooLangOption ||
-    navigator.language ||
-    defaultLanguage
-  ).toLowerCase()
-
-  const langIndex = hasLang(lang)
-    ? langs[lang]
-    : langs[defaultLanguage]
-
-  if (langIndex === langs.id && Object.prototype.hasOwnProperty.call(idI18n, key)) {
-    return idI18n[key]
+  // 优先级: translate 入参 > twikoo.init 入参 > 浏览器语言设置 > 默认语言
+  const lang = (language || twikooLangOption || navigator.language).toLowerCase()
+  let value
+  if (lang && langs[lang]) {
+    value = i18n[key][langs[lang]]
+  } else {
+    value = i18n[key][langs[defaultLanguage]]
   }
-
-  return i18n[key]?.[langIndex] || i18n[key]?.[langs.en] || ''
+  return value || ''
 }
 
 export default translate
